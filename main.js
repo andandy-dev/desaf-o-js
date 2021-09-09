0c976c117f3e3ab988ba11c5e8248f06aae58a1d
andandy-dev <eniortiz17@gmail.com>
2021-09-09 16:15:52 -0300

    desafío: condicionales JS

--- main.js
+++ main.js
@@ -1,14 +1,47 @@
-function run(){
-    let name = prompt("Please type your name")
-    let lastname = prompt("And now type your lastname")
-    let userAge = prompt("What's your age?")
-    let currentYear = 2021
-    let retirement = 65
+let countdown;
 
-    let age = parseInt(userAge)
-    let retirementPlan = retirement - age 
-    let retirementYear = currentYear + retirementPlan
+function setCountdown() {
+    countdown = parseInt(prompt("Set the minutes for the countdown"));
+    if (!(countdown > 0 && countdown < 99)) {
+        alert("set a number greater than 0 and less than 99");
+        setCountdown();
+    }
+    return countdown;
+}
 
-    alert("Hi! " + name + " " + lastname);
-    alert("Your retirement will be in " + retirementYear)
+let timeMin = setCountdown();
+let timeSec = 0;
+
+function timer() {
+    timeMin = parseInt(timeMin);
+    timeSec = parseInt(timeSec);
+    if (timeSec > 0) {
+        timeSec--;
+    } else {
+        if (timeMin > 0) {
+            timeMin--;
+            timeSec = 59;
+        } else {
+            if (timeMin == 0 && timeSec == 0) {
+                document.querySelector(".colons").classList.remove(".flicker");
+            }
+        }
+    }
+    if (timeSec < 10 || timeSec == 0) {
+        timeSec = '0' + timeSec;
+    }
+    if (timeMin < 10 || timeMin == 0) {
+        timeMin = '0' + timeMin;
+    }
+    document.querySelector(".seconds").innerHTML = timeSec;
+    document.querySelector(".minutes").innerHTML = timeMin;
+
+    if ((parseInt(timeMin) == 0) && (parseInt(timeSec) < 20)) {
+        document.querySelector(".countDown").classList.add("countOver");
+        document.querySelector(".title").innerHTML = "Countdown is almost over";
+    }
+}
+
+function run() {
+    setInterval(timer, 1000);
 }
